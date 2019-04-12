44ae469112c0b105e3b567aa101a1cf879780159
Sherri Wheeler <825423+syntaxseed@users.noreply.github.com>
2019-04-12 17:10:09 -0400

    Add attribution to boot message.

--- js/boot.js
+++ js/boot.js
@@ -3,6 +3,7 @@ var bootMessageLines = [
             (new Date()).toString()+"<br>",
             "&nbsp;_______&nbsp;&nbsp;&nbsp;______&nbsp;<br>|__&nbsp;&nbsp;&nbsp;__|&nbsp;|&nbsp;&nbsp;____|<br>&nbsp;&nbsp;&nbsp;|&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;|__&nbsp;&nbsp;&nbsp;<br>&nbsp;&nbsp;&nbsp;|&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;__|&nbsp;&nbsp;<br>&nbsp;&nbsp;&nbsp;|&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br>&nbsp;&nbsp;&nbsp;|_|&nbsp;&nbsp;&nbsp;&nbsp;|_|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br>",
             "Terminal Faker version "+version+"<br>",
+            "By Sherri Wheeler (SyntaxSeed.com)<br>",
             "Ready. Type 'help' to get started.<br><br>"
         ];
 Terminal.bootTerminalMessage(document.getElementById("terminal"), document.getElementById("boot"), bootMessageLines, 0);
