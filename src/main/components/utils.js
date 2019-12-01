59effdc8a93e2a9ad6b103ef4c42b151732a0a24
JinHuai Xuan <Jinhuai.Xuan38@myhunter.cuny.edu>
2019-12-01 14:42:54 -0500

    Fixed getAllMethodNames function to include custom commands.

--- src/main/components/utils.js
+++ src/main/components/utils.js
@@ -1,10 +1,18 @@
 export function getAllMethodNames(obj) {
   let methods = new Set();
-  while (obj = Reflect.getPrototypeOf(obj)) {
+
+  for (const x in obj) {
+    if (x != 'terminal' && x != 'commands') {
+      methods.add(x);
+    }
+  }
+
+  while(obj = Reflect.getPrototypeOf(obj)) {
     let keys = Reflect.ownKeys(obj);
     keys.forEach((k) => {
       methods.add(k);
     });
   }
+
   return [...methods];
 }
